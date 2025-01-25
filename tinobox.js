dd70e90cd04c151d0eb65c65625060ab4908d1d0
MultiLayer <multilayernowofficial@gmail.com>
2025-01-25 13:25:05 -0600

    Modify script

--- tinobox.js
+++ tinobox.js
@@ -1,24 +1,16 @@
-class LocalSave{
-  constructor(path="") {
-    this.path = window.location.hostname+path
-  }
-  set(path,data) {
-    localStorage.setItem(this.path+path,data)
-  }
-  get(path,data=null) {
-    if ( localStorage.getItem(this.path +path) !=null ) {
-      return localStorage.getItem(this.path +path)
-    } else {
-      return data
-    }
+class LocalSave {
+  constructor() {
+    console.error("Coming soon")
   }
 }
-class CoolMath{
-  wave(x= Math.PI) { return (Math.cos(x) +1) /2 }
-  randomInt(min= 0, max= 10){
+class CoolMath {
+  wave(x= Math.PI) {
+    return (Math.cos(x) +1) /2
+  }
+  randomInt(min= 0, max= 10) {
     return Math.floor(Math.random() *max) +min
   }
 }
 const coolmath= new CoolMath()
 console.log("TinoBox was imported in a script!")
-export {coolmath, LocalSave}
+export {coolmath}
